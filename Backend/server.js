316fa321a85e9fe07ea3c0a07e232866aa963012
Harsh-Kumar14 <harsh.rnhs8055@gmail.com>
2026-03-29 14:40:30 +0530

    2

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,9 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 connectToDB()
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 3000")
 })
 // if (process.env.NODE_ENV !== "production") {
